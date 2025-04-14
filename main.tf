742cd34da6a4ac5e57678eeef05f6440012b8f85
Arbatel66 <arbatel66@gmail.com>
2025-04-14 23:23:30 +1000

    adujust

--- main.tf
+++ main.tf
@@ -24,12 +24,42 @@ resource "aws_instance" "hextris-server" {
   instance_type = "t2.micro"
   key_name      = "vockey"
   user_data = file("./serve-hextris.sh")
-    
+  security_groups = [aws_security_group.hextris-server.name]
+  
   tags = {
     Name = "hextris"
+  }
+}
+
+resource "aws_security_group" "hextris-server" {
+  name        = "hextris-server"
+  description = "Hextris HTTP and SSH access"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 output "hextris-url" {
   value = aws_instance.hextris-server.public_ip
 }
+
+
+
